3499f10c077f1d2a43508e0561f35608217b774e
Renato <95065208+Renatoksh@users.noreply.github.com>
2023-08-27 21:43:35 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch31 team with vinay , pratik  and team1");
+     System.out.println("Hello batch31 team with Renato");
       System.out.println("Hello Batch batch31 WITH FOR STUDENTS RUNNING in front of Renato in making CI automated within devops CI proecss  ");
     }  
 } 
